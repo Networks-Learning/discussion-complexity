83b258396c21601a85c3139c43df2274c764452d
Utkarsh Upadhyay <musically.ut@gmail.com>
2017-07-21 18:03:31 +0200

    Add sentiment normalizing objective.

--- code/cjr/models/lin_thres.py
+++ code/cjr/models/lin_thres.py
@@ -2,7 +2,7 @@ import cvxpy as CVX
 import decorated_options as Deco
 import warnings
 import numpy as np
-from consts import COMMENTERS, VOTERS, THETA
+from consts import COMMENTERS, COMMENTER, VOTERS, THETA
 
 
 def make_thres_vars(N, M, K):
@@ -57,19 +57,19 @@ def make_constraints(topic_id_to_idx, commenter_id_to_idx, voter_id_to_idx,
     relax: Whether to frame it as an integer linear program or regular linear program.
     fixed_thres: Determines whether all thresholds are the same or different.
     """
-    aux_vars = {}
+    # aux_vars = {}
     constraints = []
 
-    for voter in model_vars['voters']:
-        for x in voter:
-            constraints.extend([0 <= x, x <= 1])
+    # for voter in model_vars['voters']:
+    #     for x in voter:
+    #         constraints.extend([0 <= x, x <= 1])
 
-    for commenter in model_vars['voters']:
-        for v in commenter:
-            constraints.extend([0 <= v, v <= 1])
+    # for commenter in model_vars['voters']:
+    #     for v in commenter:
+    #         constraints.extend([0 <= v, v <= 1])
 
-    for thres in model_vars['theta']:
-        constraints.extend([0 <= thres, thres <= 1])
+    # for thres in model_vars['theta']:
+    #     constraints.extend([0 <= thres, thres <= 1])
 
     c_vars = model_vars['commenters']
     v_vars = model_vars['voters']
@@ -88,12 +88,12 @@ def make_constraints(topic_id_to_idx, commenter_id_to_idx, voter_id_to_idx,
             constraints.extend(_upvote_cstr(x_i, v, thres))
         else:
             # Create convex constraints for downvotes on child comment
-            if relax:
-                b = CVX.Variable(name='Parent_%s_b' % trace_id)
-            else:
-                b = CVX.Int(name='Parent_%s_b' % trace_id)
+            # if relax:
+            #     b = CVX.Variable(name='Parent_%s_b' % trace_id)
+            # else:
+            #     b = CVX.Int(name='Parent_%s_b' % trace_id)
+            # aux_vars[('Parent', trace_id)] = b
 
-            aux_vars[('Parent', trace_id)] = b
             # constraints.extend(_downvote_cstr(x_i, v, thres, b))
             constraints.extend(_downvote_cstr(x_i, v, thres))
 
@@ -101,12 +101,12 @@ def make_constraints(topic_id_to_idx, commenter_id_to_idx, voter_id_to_idx,
             constraints.extend(_upvote_cstr(x_j, v, thres))
         else:
             # Create convex constraints for downvotes on child comment
-            if relax:
-                b = CVX.Variable(name='Child_%s_b' % trace_id)
-            else:
-                b = CVX.Int(name='Child_%s_b' % trace_id)
+            # if relax:
+            #     b = CVX.Variable(name='Child_%s_b' % trace_id)
+            # else:
+            #     b = CVX.Int(name='Child_%s_b' % trace_id)
+            # aux_vars[('Child', trace_id)] = b
 
-            aux_vars[('Child', trace_id)] = b
             # constraints.extend(_downvote_cstr(x_i, v, thres, b))
             constraints.extend(_downvote_cstr(x_i, v, thres))
 
@@ -114,7 +114,7 @@ def make_constraints(topic_id_to_idx, commenter_id_to_idx, voter_id_to_idx,
         for k in range(1, len(topic_id_to_idx)):
             constraints.append(theta[k] == theta[k - 1])
 
-    return constraints, aux_vars
+    return constraints # , aux_vars
 
 
 @Deco.optioned()
@@ -144,6 +144,7 @@ def make_downvote_objective(topic_id_to_idx, commenter_id_to_idx, voter_id_to_id
 
     return CVX.Maximize(obj)
 
+
 @Deco.optioned()
 def make_satisfiable():
     """An objective function which just finds a satisfiable solution."""
@@ -151,16 +152,18 @@ def make_satisfiable():
 
 
 @Deco.optioned()
-def make_improvement(truth_df, commenter_id_to_idx, noise_sigma, model_vars, seed):
+def make_improvement(truth_df, topic_id_to_idx, commenter_id_to_idx,
+                     noise_sigma, model_vars, seed):
     """Make an objective which says that the objective is to "correct" sentiment values."""
-    Y = [None] * len(commenter_id_to_idx)
+    Y = np.zeros((len(commenter_id_to_idx), len(topic_id_to_idx)), dtype=float)
     X = model_vars[COMMENTERS]
     rs = np.random.RandomState(seed=seed)
     obj = 0
-    for c_id, opinion in truth_df[truth_df.type == COMMENTERS][['id', 'opinion']].values:
+    for c_id, opinion, topic in truth_df[truth_df.type == COMMENTER][['id', 'opinion', 'topic']].values:
         c_idx = commenter_id_to_idx[int(c_id)]
-        Y[c_idx] = opinion + rs.randn() * noise_sigma
-        obj += CVX.square(X[c_idx] - Y[c_idx])
+        t_idx = topic_id_to_idx[topic]
+        Y[c_idx, t_idx] = opinion + rs.randn() * noise_sigma
+        obj += CVX.square(X[c_idx][t_idx] - Y[c_idx, t_idx])
 
+    # print(Y)
     return CVX.Minimize(obj)
-
