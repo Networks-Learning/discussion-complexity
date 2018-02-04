901a8bbcb49aaf7017b33eac45aa9936f855c104
Utkarsh Upadhyay <musically.ut@gmail.com>
2018-02-04 02:01:02 +0100

    Allow setting the memory limit while running Z3 SAT tests.

--- code/experiments/sbatch_2D_sat_run.py
+++ code/experiments/sbatch_2D_sat_run.py
@@ -14,9 +14,10 @@ OUTPUT_DIR = '/NL/stackexchange/work/matrix-completion/batch-out-nD-sat'
 @click.option('--solved-csv', 'output_csv', help='File listing already knowing statuses', default='')
 @click.option('--std-dir', 'output_dir', help='Where to save the stdout files.', default=OUTPUT_DIR)
 @click.option('--timeout', 'timeout', help='How long to let Z3 run (minutes)?', default=480)
+@click.option('--mem', 'mem', help='How much memory to allocate for the job?', default=5000)
 @click.option('--dims', 'dims', help='Number of dimensions to try to fit for.', default=2)
 @click.option('--dry/--no-dry', 'dry', help='Whether to do a dry run or a real run.', default=True)
-def cmd(ctx_compiled_csv, base_dir, output_csv, output_dir, timeout, dims, dry):
+def cmd(ctx_compiled_csv, base_dir, output_csv, output_dir, timeout, dims, dry, mem):
     """Read CTX_COMPILED_CSV, determine whether the M_partial.mat matrix in BASE_DIR has sign-rank of less than `dims` or not and save the result with the M_partial.{dims}_sat file."""
 
     df = pd.read_csv(ctx_compiled_csv)
@@ -42,19 +43,19 @@ def cmd(ctx_compiled_csv, base_dir, output_csv, output_dir, timeout, dims, dry):
 
     unknown_ctx_id = set(output_df.context_id[output_df[col_name] == 'unknown'])
 
-    for ctx_id in df.context_id.values:
-        if ctx_id in unknown_ctx_id:
-            in_file = os.path.join(base_dir, ctx_id, 'M_partial.mat')
-            op_file = in_file + '{}D_sat'.format(dims)
+    for idx, ctx_id in enumerate(unknown_ctx_id):
+        in_file = os.path.join(base_dir, ctx_id, 'M_partial.mat')
+        op_file = in_file + '{}D_sat'.format(dims)
 
-            stdout_file = f'{output_dir}/ctx{ctx_id}.{dims}D.%j'
-            sbatch_timeout = timeout + 10
-            command = f'sbatch --time={sbatch_timeout} --mem=5000 -o {stdout_file} ./sbatch_2D_sat_job.sh {in_file} {op_file} {timeout} {dims}'
+        stdout_file = f'{output_dir}/ctx{ctx_id}.{dims}D.%j'
+        sbatch_timeout = timeout + 10
+        command = f'sbatch --time={sbatch_timeout} --mem={mem} -o {stdout_file} ./sbatch_2D_sat_job.sh {in_file} {op_file} {timeout} {dims}'
 
-            if dry:
-                print(command)
-            else:
-                os.system(command)
+        print('{}/{}'.format(idx, len(unknown_ctx_id)))
+        if dry:
+            print(command)
+        else:
+            os.system(command)
 
 
 if __name__ == '__main__':
