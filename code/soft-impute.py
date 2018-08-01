dcc9a59b6f9e0b088760f53b5a0fc3e813ff1e9e
Utkarsh Upadhyay <musically.ut@gmail.com>
2018-08-01 10:45:52 +0200

    Add seed in the filename to aid in discovery.

--- code/soft-impute.py
+++ code/soft-impute.py
@@ -14,13 +14,14 @@ from fancyimpute import SoftImpute
 @click.command()
 @click.argument('in_mat_file', type=click.Path(exists=True))
 @click.option('--dims', 'dims', help='The dimensionality of the embedding.', default=2)
+@click.option('--seed', 'seed', help='The random seed to use for initializing matrices, in case initial values are not given.', default=42)
 @click.option('--suffix', 'suffix', help='Suffix to add before saving the embeddings.', default='svd')
 @click.option('-i', 'i_loo', help='Which i index to LOO.', default=-1)
 @click.option('-j', 'j_loo', help='Which j index to LOO.', default=-1)
 @click.option('--loo-output', 'loo_output', help='Where to save the LOO output.', default=None)
 @click.option('--loo-only/--no-loo-only', 'loo_only', help='Whether to only save the LOO output or whether to save the complete recovered matrix.', default=False)
 @click.option('--verbose/--no-verbose', 'verbose', help='Verbose output.', default=False)
-def cmd(in_mat_file, dims, suffix, i_loo, j_loo, loo_output, loo_only, verbose):
+def cmd(in_mat_file, dims, suffix, i_loo, j_loo, loo_output, loo_only, verbose, seed):
     """Read M_partial from IN_MAT_FILE and complete the matrix using soft-impute method."""
 
     M = io.loadmat(in_mat_file)['M_partial']
@@ -38,7 +39,7 @@ def cmd(in_mat_file, dims, suffix, i_loo, j_loo, loo_output, loo_only, verbose):
     M_complete = SoftImpute(max_rank=dims).complete(M)
 
     if LOO_mode:
-        file_tmpl = f'{in_mat_file}.r{rank}.i{i_loo}.j{j_loo}.soft-impute.out'
+        file_tmpl = f'{in_mat_file}.r{rank}.s{seed}.i{i_loo}.j{j_loo}.soft-impute.out'
 
         if not loo_only:
             op_mat_file = file_tmpl + '.mat'
